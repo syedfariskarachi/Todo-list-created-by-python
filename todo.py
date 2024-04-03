76b68a9b582175baa5eebce80e339e74f18fc8cd
syed faris <132473241+syedfariskarachi@users.noreply.github.com>
2024-04-03 14:33:13 +0500

    todo

--- todo.py
+++ todo.py
@@ -1,10 +1,10 @@
 todos = []
 condition = True
-quis1 = input("What you want to add in your todo list ?")
+quis1 = input("What you want to add in your todo list ?\n")
 print(quis1)
 todos.append(quis1)
 while condition:
-    ques2 = input("Whould you want to add more !\nif you want to exit Write 'no' ")
+    ques2 = input("Whould you want to add more !\n")
     todos.append(ques2)
     print(ques2)
     condition += 1
